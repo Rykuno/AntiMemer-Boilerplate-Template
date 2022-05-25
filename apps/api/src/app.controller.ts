5e330ba238507953a077e6118471f91ad9a6beeb
rykuno <rykuno@gmail.com>
2022-05-25 01:17:19 -0500

    feat(api): added health status code to api

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get("healthz")
+  @Get('healthz')
+  @HttpCode(200)
   healthz(): string {
-    return 'ok';
+    return "OK";
   }
 
   @Get('hello/:name')
