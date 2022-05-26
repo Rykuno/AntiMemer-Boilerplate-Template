27832d1452c2b10607b44a2e25021674988cf23f
rykuno <rykuno@gmail.com>
2022-05-26 13:09:13 -0500

    feat(api): prepared introspection/playground for production

--- apps/api/src/gql-config.service.ts
+++ apps/api/src/gql-config.service.ts
@@ -16,6 +16,7 @@ export class GqlConfigService implements GqlOptionsFactory {
       buildSchemaOptions: {
         numberScalarMode: 'integer',
       },
+      introspection: process.env.NODE_ENV !== 'production',
       // subscription
       installSubscriptionHandlers: true,
       debug: graphqlConfig.debug,
